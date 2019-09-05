3c5b6aede0c0b166a0a192ebc87c0ee9a50da7d7
Alexander Franks <afranks@gmail.com>
2019-09-05 14:20:58 -0700

    Forgot utilities function

--- utilities.R
+++ utilities.R
@@ -20,27 +20,45 @@ get_bias <- function(T, residual, alpha_hat_normalized, beta, tau_hat, w2=0, tim
     
     const <- abs(t(alpha_hat_normalized) %*% beta)
     # print(const)
+
+    w2_beta_lim <- (t(beta) %*% mvecs)[2]
     
-    w1 <- sqrt(as.numeric((const - mvals[2] * w2^2)/mvals[1]))
-    NC <- NullC(mvecs)
-    
-    bias0 <- bias1 <- 0
-    for(i in 1:times) {
-        g <- w1*mvecs[, 1] + w2 * mvecs[, 2] + sqrt(1-w1^2 - w2^2) * NC %*% rustiefel(p-2, 1)
-    
-        if(w2==0 & (abs(t(g) %*% beta) - abs(t(g) %*% alpha_hat_normalized) > 1e-10))
-            browser()
-    
+    N <- NullC(mvecs)
+    if(abs(w2) == abs(w2_lim)) {
+        g <- w1*mvecs[, 1] + w2 * mvecs[, 2]
         d <- Re(X %*% g)
         res <- glm(T ~ d, family="binomial")
         e_d <- predict(res, type="response")
                 
-        bias0 <- bias0 + sum(1 / (1-e_d[T==0]) * residual[T==0]) / sum(1 / (1-e_d[T==0]))
-        bias1 <- bias1 + sum(1 / e_d[T==1] * residual[T==1]) / sum(1 / e_d[T==1])
-
-        if(is.na(bias0) | is.na(bias1))
-            browser()
+        bias0 <- sum(1 / (1-e_d[T==0]) * residual[T==0]) / sum(1 / (1-e_d[T==0]))
+        bias1 <- sum(1 / e_d[T==1] * residual[T==1]) / sum(1 / e_d[T==1])
+        times <- 1
+    }
+    else  {
+        g <- w1*mvecs[, 1] + w2 * mvecs[, 2] + sqrt(1-w1^2 - w2^2) * N %*% rustiefel(p-2, 1)
         
+        
+        w1 <- sqrt(as.numeric((const - mvals[2] * w2^2)/mvals[1]))
+        NC <- NullC(mvecs)
+        
+        bias0 <- bias1 <- 0
+        for(i in 1:times) {
+            g <- w1*mvecs[, 1] + w2 * mvecs[, 2] + sqrt(1-w1^2 - w2^2) * NC %*% rustiefel(p-2, 1)
+            
+            if(w2==0 & (abs(t(g) %*% beta) - abs(t(g) %*% alpha_hat_normalized) > 1e-10))
+                browser()
+            
+            d <- Re(X %*% g)
+            res <- glm(T ~ d, family="binomial")
+            e_d <- predict(res, type="response")
+            
+            bias0 <- bias0 + sum(1 / (1-e_d[T==0]) * residual[T==0]) / sum(1 / (1-e_d[T==0]))
+            bias1 <- bias1 + sum(1 / e_d[T==1] * residual[T==1]) / sum(1 / e_d[T==1])
+
+            if(is.na(bias0) | is.na(bias1))
+                browser()
+            
+        }
     }
     list(bias0=bias0/times, bias1=bias1/times)
 }
