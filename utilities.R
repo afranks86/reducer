670f5bb885a775f3206ef379fc2618c39d6146ca
Alexander Franks <afranks@gmail.com>
2019-09-29 13:48:02 -0700

    updates to ipw_est for att and atc

--- utilities.R
+++ utilities.R
@@ -7,7 +7,7 @@ shrinkage_plot <- function(a, b) {
     abline(h=0)
 }
 
-gen_linearY_logisticT <- function(n, p, tau, alpha, beta, mscale, escale){
+gen_linearY_logisticT <- function(n, p, tau, alpha, beta, mscale, escale, sigma2_y){
     X <- matrix(rnorm(n * p), nrow=n, ncol=p)
     
     m <- mscale * X %*% alpha
@@ -16,7 +16,7 @@ gen_linearY_logisticT <- function(n, p, tau, alpha, beta, mscale, escale){
     e <- logistic(escale * xb)
 
     T <- rbinom(n, 1, e)
-    Y <- m +  tau * T + rnorm(n, 0, 1)
+    Y <- m +  tau * T + rnorm(n, 0, sigma2_y)
     
     list(X=X, T=T, Y=Y, m=m, xb=xb, e=e)
 }
@@ -44,11 +44,17 @@ estimate_outcome <- function(X, T, Y, cv=TRUE, Y_lambda_min=115, alpha=0){
          mhat1=mhat1)
 }
 
-estimate_propensity <- function(X, T, cv=TRUE, T_lambda_min=115, alpha=0){ 
+estimate_propensity <- function(X, T, cv=TRUE, T_lambda_min=115,
+                                eta=0.1, alpha=0){ 
     if(cv){
         cvglm <- cv.glmnet(cbind(X), T, family="binomial", 
-                           alpha=alpha, penalty.factor = rep(1, p),intercept=FALSE)
+                           alpha=alpha, penalty.factor = rep(1, p), intercept=FALSE)
         T_lambda_min <- cvglm$lambda.1se
+    }
+
+    if(eta > 1 | eta < 0) {
+        eta <- 0
+        warning(sprintf("eta  = %f", eta))
     }
     
     propensity_fit <- glmnet(cbind(X), T, family="binomial", 
@@ -58,11 +64,17 @@ estimate_propensity <- function(X, T, cv=TRUE, T_lambda_min=115, alpha=0){
     escale_hat <- sqrt(sum(beta_hat^2))
     beta_hat_normalized <- beta_hat / escale_hat
     ehat <- predict(propensity_fit, type="response", newx=X)
+
+    ## Clipping
+    ehat_clip <- ehat
+    ehat_clip[ehat < eta] <- eta
+    ehat_clip[ehat > 1 - eta] <- 1 - eta
     
     list(beta_hat=beta_hat,
          beta_hat_normalized=beta_hat_normalized,
          escale_hat=escale_hat,
-         ehat=ehat)
+         ehat=ehat,
+         ehat_clip=ehat_clip)
 }
 
 # Grabs named attribute from list if not na; else returns default
@@ -80,6 +92,7 @@ compute_gammas <- function(ab_dot_prod, mvals, mvecs, w2, times){
     # Stop if magnitudes of w1 and w2 are too large, and likely not due to numerical error.
     # Inserted so that max statement in next line does not silence bugs.
     stopifnot(1 - w1^2 - w2^2 > -1e-6)
+        
     gammas <- w1 * mvecs[, 1] + w2 * mvecs[, 2] + sqrt(max(1 - w1^2 - w2^2, 0)) * null_vecs
     gammas
 }
@@ -125,81 +138,6 @@ get_bias_vec <- function(T, Y, X, xb, mvecs, mvals, ab_dot_prod, escale, w2=0, w
     #     c1s=c1s, dd=dd, dd_normalized=dd_normalized)
 }
 
-get_bias_old <- function(T, Y, X, xb, mvecs, mvals, ab_dot_prod, escale, w2=0, w2lim, times=10,
-                     DEBUG=FALSE, alpha_hat_normalized=NA, beta_hat_normalized=NA) {
-    
-    N <- NullC(mvecs)
-    
-    # Debugging leftovers 
-    #reductions <- get_bias_vec(T, Y, X, xb, mvecs, mvals, ab_dot_prod, escale, w2, w2lim, times)
-    #gg <- reductions$gammas
-    #e_dd <- reductions$e_dd
-    #ests <- reductions$ests
-    #debug_integrand <- reductions$integrand
-
-    w1 <- sqrt(as.numeric((ab_dot_prod - mvals[2] * w2^2)/mvals[1]))
-    bias0 <- bias1 <- 0
-    normal_samples <- rnorm(500, sd=1)
-
-    for(i in 1:times) {
-        # Stop if magnitudes of w1 and w2 are too large, and likely not due to numerical error.
-        # Inserted so that max statement in next line does not silence bugs.
-        stopifnot(1 - w1^2 - w2^2 > -1e-6)
-        g <- w1 * mvecs[, 1] + w2 * mvecs[, 2] + sqrt(max(1 - w1^2 - w2^2, 0)) * N[, i]# %*% rustiefel(p-2, 1)
-        d <- Re(X %*% g)
-        
-        if(DEBUG){
-            if(is.na(alpha_hat_normalized) || is.na(beta_hat_normalized))
-                stop("Debug requires alpha_hat_normalized and beta_hat_normalized vectors.")
-            mhatX <- X %*% alpha_hat_normalized
-            eX <- X %*% beta_hat_normalized
-            
-            print(sprintf("Partial correlation: %s",
-                          round(cor(lm(mhatX ~ d)$resid, lm(eX ~ d)$resid), 4)))
-            
-            print(sprintf("Hyperbola condition: %s",
-                          round((alpha_hat_normalized %*% as.vector(g)) *
-                                  (as.vector(g) %*% beta_hat_normalized) -
-                                    (alpha_hat_normalized %*% beta_hat_normalized), 4)))
-           
-            if(w2 == -sqrt(abs(mvals[2])))
-              print(sprintf("Checking that w2 = min corresponds to e(X): %s",
-                            round(cor(g, beta_hat_normalized), 4)))
-            if(w2 == sqrt(abs(mvals[2])))
-              print(sprintf("Checking that w2 = max corresponds to mhat(X): %s",
-                            round(cor(g, alpha_hat_normalized), 4)))
-            if(w2 == 0)
-              print(sprintf("Checking that w2 = 0 correspond to bisector: %s",
-                            round(sum(alpha_hat_normalized %*% as.vector(g) -
-                                          beta_hat_normalized %*% as.vector(g)), 4)))
-        }
-
-        c1 <- as.numeric(t(xb/sqrt(sum(xb^2))) %*% d/sqrt(sum(d^2)))
-        # Stop if magnitude of c1 is too large, and likely not due to numerical error.
-        # Inserted so that max statement in next line does not silence bugs.
-        stopifnot(1 - c1^2 > -1e-6)
-        c2 <- sqrt(max(1-c1^2, 0))
-
-        e_d <- sapply(d, function(di) {
-            mean(invlogit(escale*(c1*di + c2*normal_samples), a=0, b=1))
-        })
-        
-        ## res <- glm(T ~ d - 1, family="binomial")
-        ## e_d <- predict(res, type="response")
-        
-        bias0 <- bias0 + sum(1 / (1-e_d[T==0]) * Y[T==0]) / sum(1 / (1-e_d[T==0]))
-        bias1 <- bias1 + sum(1 / e_d[T==1] * Y[T==1]) / sum(1 / e_d[T==1])
-
-        if(is.na(bias0) | is.na(bias1))
-            browser()
-        #biases[i] <- mybias1 - mybias0
-        
-    }
-
-    #print(summary(biases))
-    list(bias0=bias0/times, bias1=bias1/times)
-}
-
 invlogit <- function(x, a, b) {
     exp(a+x*b) / (1+exp(a+x*b))
 }
@@ -209,8 +147,16 @@ logistic <- function(x){
     #1 / (1 + exp(-x))
 }
 
-ipw_est <- function(e, T, Y, hajek=FALSE){
-  wts <- (T / e) - ((1 - T) / (1 - e))
+ipw_est <- function(e, T, Y, estimand, hajek=FALSE){
+
+    if(estimand == "ATT") {
+        wts <- T - (1-T) * e / (1-e) 
+    } else if (estimand == "ATC") {
+        wts <- T * (1-e) / e - (1-T)
+    } else {
+        wts <- (T / e) - ((1 - T) / (1 - e))
+    }
+
   if(hajek) {
     # Note that negative weights for control assignments are flipped to positive by normalization
     sum(wts[T == 1] * Y[T == 1]) / sum(wts[T == 1]) - sum(wts[T == 0] * Y[T == 0]) / sum(wts[T == 0])
